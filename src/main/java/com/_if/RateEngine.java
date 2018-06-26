6980c8b202f54f438d5b9ded2b6e9ec41a881a83
Ideen Faraji <ideen.faraji@servian.com.au>
2018-06-26 22:19:28 +1000

    early bird - initial impl

--- src/main/java/com/_if/RateEngine.java
+++ src/main/java/com/_if/RateEngine.java
@@ -1,5 +1,19 @@
 package com._if;
 
-public class RateEngine {
+import java.util.Calendar;
+import com._if.model.Rate;
 
+public class RateEngine {
+	public Rate calculate(Calendar entry, Calendar exit){
+		Rate rate = null;
+		if (isEarlyBird(entry, exit)){
+			rate = new Rate("EarlyBird", 15d);
+		}
+			
+		return rate;
+	}
+	
+	private boolean isEarlyBird(Calendar entry, Calendar exit){
+		return false;
+	}
 }
